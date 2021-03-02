dcef107b5aa64407eaf900f273877856301a9401
Lucy Gorbachova <xa12850503@student.karazin.ua>
2021-03-02 19:46:47 +0200

    2

---
+++ unit-1/config.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Simple Jar
+echo. 
+java -jar D:\NIX-4\unit-1\simple_jar\build\jar\SimpleProgram.jar 
+echo.
+echo. 
+echo Maven Jar
+java -jar D:\NIX-4\unit-1\maven_with_libs\target\MavenPractice-1.0-SNAPSHOT.jar 
+echo.
+echo. 
+echo Ant Jar
+java -jar D:\NIX-4\unit-1\ant_with_libs\build\jar\main.jar 
+pause
+
